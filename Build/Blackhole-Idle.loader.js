11d6658a7a4d39f440efe1ed8245c724fe76fde3
iEagle4303 <169015802+iEagle4303@users.noreply.github.com>
2025-01-12 11:43:03 -0600

    Working Save

--- Build/Blackhole-Idle.loader.js
+++ Build/Blackhole-Idle.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 77525987,
+    wasmFileSize: 77525980,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
